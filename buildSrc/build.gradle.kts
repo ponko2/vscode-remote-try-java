91f5c740bf8f318e58a158240ac2677775dfd49a
Takahito Nakano <kano@ponko2.jp>
2022-02-28 01:21:05 +0900

    ci: Spotlessの設定を追加

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 }
 
 dependencies {
+  implementation("com.diffplug.spotless:spotless-plugin-gradle:6.3.0")
   implementation("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
   implementation("net.ltgt.gradle:gradle-errorprone-plugin:2.0.2")
   implementation("org.springframework.boot:spring-boot-gradle-plugin:2.6.4")
