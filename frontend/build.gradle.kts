46776b799491901bcd202b1c7356381309f6f954
Takahito Nakano <kano@ponko2.jp>
2022-04-07 21:55:00 +0900

    build: マルチプロジェクトビルドに変更

---
+++ frontend/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.siouan.frontendgradleplugin.infrastructure.gradle.AssembleTask
+import org.siouan.frontendgradleplugin.infrastructure.gradle.InstallDependenciesTask
+
+plugins {
+  alias(libs.plugins.frontend)
+}
+
+frontend {
+  nodeDistributionProvided.set(true)
+  packageJsonDirectory.set(rootProject.projectDir)
+  assembleScript.set("-w frontend run build")
+  cleanScript.set("-w frontend run clean")
+  installScript.set("ci")
+}
+
+tasks.named<InstallDependenciesTask>("installFrontend").configure {
+  inputs.files(
+    "package.json",
+    "${rootProject.projectDir}/package.json",
+    "${rootProject.projectDir}/package-lock.json"
+  )
+  outputs.dir("${rootProject.projectDir}/node_modules")
+}
+
+tasks.named<AssembleTask>("assembleFrontend").configure {
+  inputs.files(
+    "package.json",
+    "${rootProject.projectDir}/package.json",
+    "${rootProject.projectDir}/package-lock.json"
+  )
+  inputs.dir("src")
+  outputs.dir("dist")
+}
