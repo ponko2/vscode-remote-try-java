d7ebffe1bc96d7927ba4982da46912a3786052cb
Takahito Nakano <kano@ponko2.jp>
2022-04-09 19:44:21 +0900

    ci: Spotlessの設定を追加

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -2,4 +2,5 @@ export default {
   "*": "secretlint",
   "*.{css,json,scss,yml}": "prettier --write",
   "*.{js,jsx,mjs,ts,tsx}": ["eslint --fix", "prettier --write"],
+  "*.{gradle.kts,java}": () => "./gradlew spotlessApply",
 };
