7614d50b18633775bec438c7349bc3ec83fcc0c1
Takahito Nakano <kano@ponko2.jp>
2024-01-19 20:29:32 +0900

    ci: 非推奨な設定を修正

--- frontend/.storybook/test-runner.ts
+++ frontend/.storybook/test-runner.ts
@@ -1,9 +1,9 @@
 import type { TestRunnerConfig } from "@storybook/test-runner";
 
 const config: TestRunnerConfig = {
-  async postRender(page, context) {
+  async postVisit(page, context) {
     const elementHandler = await page.$("#storybook-root");
-    const innerHTML = await elementHandler.innerHTML();
+    const innerHTML = await elementHandler?.innerHTML();
     expect(innerHTML).toMatchSnapshot();
   },
 };
